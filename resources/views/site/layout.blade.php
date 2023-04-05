020bb403064c3bae3a9458f4da464a9f9fd454b1
4lbert4rgolo <albertsilvadeargolo@gmail.com>
2023-04-05 17:16:15 -0300

    New project

---
+++ resources/views/site/layout.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <!-- Compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    
+</head>
+<body>
+
+  <!-- Dropdown Structure -->
+  <ul id='dropdown1' class='dropdown-content'>
+    @foreach ($categoriasMenu as $categoriaM)
+    <li><a href="{{ route('site.categoria', $categoriaM->id)}}">{{$categoriaM->nome}}</a></li>
+    @endforeach
+  
+
+  </ul>
+
+    <nav class="blue">
+        <div class="nav-wrapper container">
+          <a href="#" class="brand-logo center">NewApp</a>
+          <ul id="nav-mobile" class="left">
+            <li><a href="{{ route('site.index')}}">Home</a></li>
+            <li><a href="#" class="dropdown-trigger" data-target='dropdown1'>Categorias <i class="material-icons right">expand_more</i></a></li>
+            <li><a href="">Carrinho</a></li>
+            
+          </ul>
+        </div>
+      </nav>
+
+@yield('conteudo')
+
+ <!-- Compiled and minified JavaScript -->
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>      
+ 
+ <script>
+  /* Dropdown */
+var elemDrop = document.querySelectorAll('.dropdown-trigger');
+var instanceDrop = M.Dropdown.init(elemDrop, {
+    coverTrigger: false,
+    constrainWidth: false
+});
+ </script>
+
+</body>
+</html>
